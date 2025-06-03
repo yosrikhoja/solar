7950588ee2b9281ba47c791b2a8e90cf0e8b2d08
yosrikhoja <khojayosri@gmail.com>
2025-06-03 22:39:51 +0100

    fix test phase in pipeline

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+};
